4bbcc1c68aa2dc54f5aeeb5ecb7ff623aae1d5e8
Spencer14420 <67592325+Spencer14420@users.noreply.github.com>
2023-03-23 17:51:04 -0400

    Typo and missing require

--- includes/formProcessors/login.php
+++ includes/formProcessors/login.php
@@ -1,6 +1,6 @@
 <?php
 require_once __DIR__ . "/../standardReq.php";
-require_once __DIR__ . "/../classes/user.php";
+require_once __DIR__ . "/../classes/User.php";
 
 //Check if username does not exist or is deleted
 $user = new User($_POST['username']);
