eafc89afa7fc1cec9cfdd86c39dd36731eaca883
Spencer14420 <67592325+Spencer14420@users.noreply.github.com>
2023-08-28 12:54:15 -0400

    Check if the confirmation box is checked

--- includes/formProcessors/undeleteUser.php
+++ includes/formProcessors/undeleteUser.php
@@ -3,6 +3,10 @@
 require_once __DIR__ . "/../standardReq.php";
 require_once __DIR__ . "/../classes/EditableUser.php";
 
+if (!isset($_POST['confirmed'])) {
+    exit("You must check the box to confirm");
+}
+
 //Create user object for user being edited
 $selectedUser = new EditableUser($_GET['user']);
 
