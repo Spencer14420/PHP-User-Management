f6ef6fc20d42393ded936ed8eaa119da64b872b6
Spencer14420 <67592325+Spencer14420@users.noreply.github.com>
2023-09-05 11:23:04 -0400

    Demo unit test

---
+++ tests/UnitTest.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+require_once __DIR__ . "/auth.php";
+require_once __DIR__ . "/../config/mysql.php";
+require_once __DIR__ . "/../config/systemSettings.php";
+require_once __DIR__ . "/../includes/classes/User.php";
+
+use PHPUnit\Framework\TestCase;
+
+final class UnitTest extends TestCase
+{
+    public function test(): void
+    {
+        $user = new User("User1");
+
+        $testExists = $user->exists;
+
+        $this->assertSame(false, $testExists);
+    }
+}
