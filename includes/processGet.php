b109651d8bd4e8ff157d1dd9b8b7840906d1e815
Spencer14420 <67592325+Spencer14420@users.noreply.github.com>
2023-09-07 19:36:46 -0400

    Fix an issue which allowed users without the view permission to check if an account exists (and fix a typo)

--- includes/processGet.php
+++ includes/processGet.php
@@ -3,111 +3,119 @@
 require_once __DIR__ . "/standardReq.php";
 require_once __DIR__ . "/forms.php";
 
+if (!isset($_GET['action'])) {
+    exit();
+}
 
-if (isset($_GET['action'])) {
-    //Createaccount form
-    if ($_GET['action'] === "createaccount") {
-        if ($nopassMode) {
-            $nopasscreateaccountForm->setAction("index.php?action=createaccount");
-            $nopasscreateaccountForm->echoForm();
-        } else {
-            $createaccountForm->setAction("index.php?action=createaccount");
-            $createaccountForm->echoForm();
-        }
-        //Login forms
-    } elseif ($_GET['action'] === "login") {
-        if ($nopassMode) {
-            //Nopass login form
-            $nopassloginForm->setAction("index.php?action=login&verify=1");
-            $nopassloginForm->echoForm();
-        } else {
-            //Regular login form
-            $loginForm->setAction("index.php?action=login");
-            $loginForm->echoForm();
-        }
-        //User list
-    } elseif ($_GET['action'] === "userlist") {
-        include_once __DIR__ . "/userlist.php";
-        //Request account form
-    } elseif ($_GET['action'] === "requestaccount") {
-        $requestAccountForm->setAction("index.php?action=requestaccount");
-        $requestAccountForm->echoForm();
-        //Requested accounts panel
-    } elseif ($_GET['action'] === "reviewrequested") {
-        include_once __DIR__ . "/reqAccounts.php";
-        //Renameuser form
-    } elseif (isset($_GET['user']) and $_GET['action'] === "rename") {
-        $user = new User($_GET['user']);
-        if ($user->exists) {
-            echo "Rename <b>$user->formattedUsername</b><br><br>";
-            $renameUserForm->setAction("index.php?action=rename&user=$user->username");
-            //addCSRF parameter is the name of the session variable
-            //Below sets a unique $_SESSION variable name based on GET variables,
-            //so multiple actions can be performed at the same time
-            //without overwriting each others' tokens
-            $renameUserForm->addCSRF($_GET['action'] . $user->username);
-            $renameUserForm->echoForm();
-        } else {
-            echo "The user you have selected does not exist";
-        }
-        //Renameuser form
-    } elseif (isset($_GET['user']) and $_GET['action'] === "editemail") {
-        $user = new User($_GET['user']);
-        if ($user->exists) {
-            echo "Edit <b>$user->formattedUsername's</b> email address<br><br>";
-            echo "Current email address: <b>$user->sanitizedEmail</b><br><br>";
-            $editEmailForm->setAction("index.php?action=editemail&user=$user->username");
-            //addCSRF parameter is the name of the session variable
-            //Below sets a unique $_SESSION variable name based on GET variables,
-            //so multiple actions can be performed at the same time
-            //without overwriting each others' tokens
-            $editEmailForm->addCSRF($_GET['action'] . $user->username);
-            $editEmailForm->echoForm();
-        } else {
-            echo "The user you have selected does not exist";
-        }
-        //Edit groups form
-    } elseif (isset($_GET['user']) and $_GET['action'] === "editgroups") {
-        $user = new User($_GET['user']);
-        if ($user->exists) {
-            echo "Add or remove <b>$user->formattedUsername</b> from groups<br><br>";
-            //Defined in forms.php
-            renderEditGroupsForm($user->username);
-        } else {
-            echo "The user you have selected does not exist";
-        }
-        //Delete user form
-    } elseif (isset($_GET['user']) and $_GET['action'] === "delete") {
-        $user = new User($_GET['user']);
-        if ($user->exists) {
-            echo "Delete <b>$user->formattedUsername</b><br><br>";
-            $deleteUserForm->setAction("index.php?action=delete&user=$user->username");
-            $deleteUserForm->addCSRF($_GET['action'] . $user->username);
-            $deleteUserForm->echoForm();
-        } else {
-            echo "The user you have selected does not exist";
-        }
-        //Undelete user form
-    } elseif (isset($_GET['user']) and $_GET['action'] === "undelete") {
-        $user = new User($_GET['user']);
-        if ($user->exists) {
-            echo "Undelete <b>$user->formattedUsername</b><br><br>";
-            $undeleteUserForm->setAction("index.php?action=undelete&user=$user->username");
-            $undeleteUserForm->addCSRF($_GET['action'] . $user->username);
-            $undeleteUserForm->echoForm();
-        } else {
-            echo "The user you have selected does not exist";
-        }
-    } elseif (isset($_GET['user']) and $_GET['action'] === "declinereq") {
-        include_once __DIR__ . "/classes/RequestedAccount.php";
-        $request = new RequestedAccount($_GET['user']);
-        if ($request->exists) {
-            echo "Decline <b>$request->sanitizedUsername</b> ($request->sanitizedEmail)<br><br>";
-            $declineReqAcc->setAction("index.php?action=declinereq&user=$request->username");
-            $declineReqAcc->addCSRF($_GET['action'] . $request->username);
-            $declineReqAcc->echoForm();
-        } else {
-            echo "An account with the username you have selected has not been requested";
-        }
+//Createaccount form
+if ($_GET['action'] === "createaccount") {
+    if ($nopassMode) {
+        $nopasscreateaccountForm->setAction("index.php?action=createaccount");
+        $nopasscreateaccountForm->echoForm();
+    } else {
+        $createaccountForm->setAction("index.php?action=createaccount");
+        $createaccountForm->echoForm();
+    }
+    //Login forms
+} elseif ($_GET['action'] === "login") {
+    if ($nopassMode) {
+        //Nopass login form
+        $nopassloginForm->setAction("index.php?action=login&verify=1");
+        $nopassloginForm->echoForm();
+    } else {
+        //Regular login form
+        $loginForm->setAction("index.php?action=login");
+        $loginForm->echoForm();
+    }
+    //Request account form
+} elseif ($_GET['action'] === "requestaccount") {
+    $requestAccountForm->setAction("index.php?action=requestaccount");
+    $requestAccountForm->echoForm();
+} else {
+    //Check for "view" permission (to protect the actions below)
+    require_once __DIR__ . "/pageCheck.php";
+}
+
+//Actions that require the "view" permission are listed below 
+
+//User list
+if ($_GET['action'] === "userlist") {
+    include_once __DIR__ . "/userlist.php";
+    //Requested accounts panel
+} elseif ($_GET['action'] === "reviewrequested") {
+    include_once __DIR__ . "/reqAccounts.php";
+    //Renameuser form
+} elseif (isset($_GET['user']) and $_GET['action'] === "rename") {
+    $user = new User($_GET['user']);
+    if ($user->exists) {
+        echo "Rename <b>$user->formattedUsername</b><br><br>";
+        $renameUserForm->setAction("index.php?action=rename&user=$user->username");
+        //addCSRF parameter is the name of the session variable
+        //Below sets a unique $_SESSION variable name based on GET variables,
+        //so multiple actions can be performed at the same time
+        //without overwriting each others' tokens
+        $renameUserForm->addCSRF($_GET['action'] . $user->username);
+        $renameUserForm->echoForm();
+    } else {
+        echo "The user you have selected does not exist";
+    }
+    //Renameuser form
+} elseif (isset($_GET['user']) and $_GET['action'] === "editemail") {
+    $user = new User($_GET['user']);
+    if ($user->exists) {
+        echo "Edit <b>$user->formattedUsername's</b> email address<br><br>";
+        echo "Current email address: <b>$user->sanitizedEmail</b><br><br>";
+        $editEmailForm->setAction("index.php?action=editemail&user=$user->username");
+        //addCSRF parameter is the name of the session variable
+        //Below sets a unique $_SESSION variable name based on GET variables,
+        //so multiple actions can be performed at the same time
+        //without overwriting each others' tokens
+        $editEmailForm->addCSRF($_GET['action'] . $user->username);
+        $editEmailForm->echoForm();
+    } else {
+        echo "The user you have selected does not exist";
+    }
+    //Edit groups form
+} elseif (isset($_GET['user']) and $_GET['action'] === "editgroups") {
+    $user = new User($_GET['user']);
+    if ($user->exists) {
+        echo "Add or remove <b>$user->formattedUsername</b> from groups<br><br>";
+        //Defined in forms.php
+        renderEditGroupsForm($user->username);
+    } else {
+        echo "The user you have selected does not exist";
+    }
+    //Delete user form
+} elseif (isset($_GET['user']) and $_GET['action'] === "delete") {
+    $user = new User($_GET['user']);
+    if ($user->exists) {
+        echo "Delete <b>$user->formattedUsername</b><br><br>";
+        $deleteUserForm->setAction("index.php?action=delete&user=$user->username");
+        $deleteUserForm->addCSRF($_GET['action'] . $user->username);
+        $deleteUserForm->echoForm();
+    } else {
+        echo "The user you have selected does not exist";
+    }
+    //Undelete user form
+} elseif (isset($_GET['user']) and $_GET['action'] === "undelete") {
+    $user = new User($_GET['user']);
+    if ($user->exists) {
+        echo "Undelete <b>$user->formattedUsername</b><br><br>";
+        $undeleteUserForm->setAction("index.php?action=undelete&user=$user->username");
+        $undeleteUserForm->addCSRF($_GET['action'] . $user->username);
+        $undeleteUserForm->echoForm();
+    } else {
+        echo "The user you have selected does not exist";
+    }
+} elseif (isset($_GET['user']) and $_GET['action'] === "declinereq") {
+    include_once __DIR__ . "/classes/RequestedAccount.php";
+    $request = new RequestedAccount($_GET['user']);
+    if ($request->exists) {
+        echo "Decline <b>$request->sanitizedUsername</b> ($request->sanitizedEmail)<br><br>";
+        $declineReqAcc->setAction("index.php?action=declinereq&user=$request->username");
+        $declineReqAcc->addCSRF($_GET['action'] . $request->username);
+        $declineReqAcc->echoForm();
+    } else {
+        echo "An account with the username you have selected has not been requested";
     }
 }
--- includes/userlist.php
+++ includes/userlist.php
@@ -3,9 +3,6 @@ require_once __DIR__ . "/auth.php";
 require_once __DIR__ . "/../config/mysql.php";
 include_once __DIR__ . "/userHeader.php";
 
-//Check for "view" permission
-require_once __DIR__ . "/pageCheck.php";
-
 $query = $mysqli->prepare("SELECT username FROM users");
 $query->execute();
 $result = $query->get_result();
@@ -25,16 +22,16 @@ while ($row = $result->fetch_assoc()) {
     echo ($user->email) ? " ($user->sanitizedEmail)" : "";
 
     if ($currentUser->hasPerm("renameusers")) {
-        echo " <a href=index.php?action=rename&user=$$user->sanitizedUsername>(Rename)</a>";
+        echo " <a href=index.php?action=rename&user=$user->sanitizedUsername>(Rename)</a>";
     }
     if ($currentUser->hasPerm("{$deleteAction}users")) {
-        echo " <a href=index.php?action=$deleteAction&user=$$user->sanitizedUsername>(" . ucfirst($deleteAction) . ")</a>";
+        echo " <a href=index.php?action=$deleteAction&user=$user->sanitizedUsername>(" . ucfirst($deleteAction) . ")</a>";
     }
     if ($currentUser->hasPerm("groupusers")) {
-        echo " <a href=index.php?action=editgroups&user=$$user->sanitizedUsername>(Edit groups)</a>";
+        echo " <a href=index.php?action=editgroups&user=$user->sanitizedUsername>(Edit groups)</a>";
     }
     if ($currentUser->hasPerm("editemail")) {
-        echo " <a href=index.php?action=editemail&user=$$user->sanitizedUsername>(Edit email)</a>";
+        echo " <a href=index.php?action=editemail&user=$user->sanitizedUsername>(Edit email)</a>";
     }
     echo "<br><br>";
 }
