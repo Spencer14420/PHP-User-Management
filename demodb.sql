a69e93562716d09eba5352cbee8fb32db3d2b8d5
Spencer14420 <67592325+Spencer14420@users.noreply.github.com>
2023-09-07 13:22:10 -0400

    fix

--- demodb.sql
+++ demodb.sql
@@ -2,7 +2,7 @@ SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
 
-CREATE DATABASE phpuser
+CREATE DATABASE phpuser;
 
 CREATE TABLE `groups` (
   `id` int(11) NOT NULL,
