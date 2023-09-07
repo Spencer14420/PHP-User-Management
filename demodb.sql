0b5f913cc28dedbba89e68124156422fc604fb2a
Spencer14420 <67592325+Spencer14420@users.noreply.github.com>
2023-09-07 13:13:37 -0400

    upload sql create demo database

---
+++ demodb.sql
@@ -0,0 +1,109 @@
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+CREATE DATABASE phpuser
+
+CREATE TABLE `groups` (
+  `id` int(11) NOT NULL,
+  `groupname` text NOT NULL,
+  `userid` int(11) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `groups`
+--
+
+INSERT INTO `groups` (`id`, `groupname`, `userid`) VALUES
+(1, 'admin', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `req_accounts`
+--
+
+CREATE TABLE `req_accounts` (
+  `id` int(11) NOT NULL,
+  `email` text NOT NULL,
+  `name` text NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `req_accounts`
+--
+
+INSERT INTO `req_accounts` (`id`, `email`, `name`) VALUES
+(1, 'requestedaccount@example.com', 'RequestedUsername');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `username` text NOT NULL,
+  `password` text NOT NULL,
+  `token` text NOT NULL,
+  `email` text NOT NULL,
+  `deleted` tinyint(4) NOT NULL DEFAULT 0
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `password`, `token`, `email`, `deleted`) VALUES
+(1, 'Admin', '0', '0', 'admin@example.com', 0),
+(2, 'User', '0', '0', 'user@example.com', 0);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `groups`
+--
+ALTER TABLE `groups`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `req_accounts`
+--
+ALTER TABLE `req_accounts`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `groups`
+--
+ALTER TABLE `groups`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
+
+--
+-- AUTO_INCREMENT for table `req_accounts`
+--
+ALTER TABLE `req_accounts`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
