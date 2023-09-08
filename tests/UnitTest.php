08e1a1dcb5102c73559f0bc6a458731fc199798e
Spencer14420 <67592325+Spencer14420@users.noreply.github.com>
2023-09-08 17:04:02 -0400

    Fix error making global variables null when running PHPUnit?

--- tests/UnitTest.php
+++ tests/UnitTest.php
@@ -2,17 +2,20 @@
 
 declare(strict_types=1);
 
-require_once __DIR__ . "/../src/includes/auth.php";
-require_once __DIR__ . "/../src/config/mysql.php";
-require_once __DIR__ . "/../src/config/systemSettings.php";
-require_once __DIR__ . "/../src/includes/classes/User.php";
-
 use PHPUnit\Framework\TestCase;
 
 final class UnitTest extends TestCase
 {
     public function test(): void
     {
+        require_once __DIR__ . "/../src/config/mysql.php";
+
+        $GLOBALS['mysqli'] = $mysqli;
+
+        require_once __DIR__ . "/../src/includes/auth.php";
+        require_once __DIR__ . "/../src/config/systemSettings.php";
+        require_once __DIR__ . "/../src/includes/classes/User.php";
+
         $user = new User("User1");
 
         $testExists = $user->exists;
