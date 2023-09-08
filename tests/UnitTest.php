b0dc0ce8de56cadec6abba0b79b1160c48deec1c
Spencer14420 <67592325+Spencer14420@users.noreply.github.com>
2023-09-08 11:46:36 -0400

    fix

--- tests/UnitTest.php
+++ tests/UnitTest.php
@@ -2,10 +2,10 @@
 
 declare(strict_types=1);
 
-require_once __DIR__ . "/../includes/auth.php";
-require_once __DIR__ . "/../config/mysql.php";
-require_once __DIR__ . "/../config/systemSettings.php";
-require_once __DIR__ . "/../includes/classes/User.php";
+require_once __DIR__ . "/src/includes/auth.php";
+require_once __DIR__ . "/src/config/mysql.php";
+require_once __DIR__ . "/src/config/systemSettings.php";
+require_once __DIR__ . "/src/includes/classes/User.php";
 
 use PHPUnit\Framework\TestCase;
 
