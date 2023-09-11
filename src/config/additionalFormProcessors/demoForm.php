2d49a02b62d06ccbc88b78c1c5924d41ff3e44f3
Spencer14420 <67592325+Spencer14420@users.noreply.github.com>
2023-09-11 19:33:36 -0400

    Start custom form processing

---
+++ src/config/additionalFormProcessors/demoForm.php
@@ -0,0 +1,2 @@
+<?
+echo "demo form processor";
--- src/config/additionalForms.php
+++ src/config/additionalForms.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once __DIR__ . "/../includes/classes/Form.php";
+require_once __DIR__ . "/../includes/classes/CustomForm.php";
 
-$demoForm = new Form();
+$demoForm = new CustomForm("demoForm");
 $demoForm->addInput("text", "demoInput", "Demo Input", true, "");
