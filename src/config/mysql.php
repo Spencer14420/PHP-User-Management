3fb20e96a78f6668db9f60cf00874fc0d1d87252
Spencer14420 <67592325+Spencer14420@users.noreply.github.com>
2023-09-13 15:15:34 -0400

    needed

--- src/config/mysql.php
+++ src/config/mysql.php
@@ -2,6 +2,8 @@
 
 $mysqli = new mysqli("localhost", "root", "root", "phpuser");
 
+$GLOBALS['mysqli'] = $mysqli;
+
 if ($mysqli->connect_errno) {
     echo "Failed to connect to MySQL: " . $mysqli->connect_error;
     exit();
